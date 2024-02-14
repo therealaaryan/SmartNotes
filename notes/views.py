140cfb5846ecab007d723d0f4898005ca97bcd70
Aaryan Mehta <aaryanmta@gmail.com>
2024-02-14 18:49:07 +0530

    dynamic and static web pages

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Notes
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+    template_name = "notes/notes_details.html"
